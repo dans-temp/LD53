d685c1e10a0a30008dc103be580cf0fea4939fe9
dans-temp <122122126+dans-temp@users.noreply.github.com>
2023-04-30 23:52:22 -0600

    Delete game directory

--- game/globals.js
+++
@@ -1,6 +0,0 @@
-export const WIDTH_CANVAS = 1920;
-export const HEIGHT_CANVAS = 1080;
-export const PADDING_CANVAS = 20;
-
-export const FONT_TITLE = "JosefinSlab";
-export const FONT_DEFAULT = "JosefinSlab";
