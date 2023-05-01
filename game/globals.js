1c39320c5337dd4e573b577223b7ceebf1b8438e
dans-temp <122122126+dans-temp@users.noreply.github.com>
2023-04-30 18:27:35 -0600

    Add files via upload

---
+++ game/globals.js
@@ -0,0 +1,6 @@
+export const WIDTH_CANVAS = 1920;
+export const HEIGHT_CANVAS = 1080;
+export const PADDING_CANVAS = 20;
+
+export const FONT_TITLE = "JosefinSlab";
+export const FONT_DEFAULT = "JosefinSlab";
